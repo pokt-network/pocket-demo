781a9effdd431e49447bae64bbc94d7ab84742e7
Pabel Nunez Landestoy <pnunezl@elevensystems.com>
2017-11-27 18:55:32 -0400

    Initial commit

---
+++ PocketDemo/DUBER/Services/BaseOperation.swift
@@ -0,0 +1,9 @@
+//
+//  BaseOperation.swift
+//  PocketDemo
+//
+//  Created by mac on 11/27/17.
+//  Copyright © 2017 Pabel Nunez Landestoy. All rights reserved.
+//
+
+import Foundation
---
+++ PocketDemo/DUBER/Services/TransactionPostOperation.swift
@@ -0,0 +1,9 @@
+//
+//  TransactionPostOperation.swift
+//  PocketDemo
+//
+//  Created by mac on 11/27/17.
+//  Copyright © 2017 Pabel Nunez Landestoy. All rights reserved.
+//
+
+import Foundation
